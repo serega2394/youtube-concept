e4e9c249d21bae2e449a35d2c6ae7a3e284ee582
serega2394 <88282224+serega2394@users.noreply.github.com>
2021-08-01 18:38:29 +0300

    Сверстан проект нового Youtube

---
+++ js/main.js
@@ -0,0 +1,88 @@
+const channel = new Swiper(".channel-slider", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: "#channel-button-next",
+    prevEl: "#channel-button-prev",
+  },
+});
+
+const recommended = new Swiper(".recommended-slider", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 2,
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: "#recommended-button-next",
+    prevEl: "#recommended-button-prev",
+  },
+});
+
+const food_drink = new Swiper(".food-drink-slider", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1350: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: "#food-drink-button-next",
+    prevEl: "#food-drink-button-prev",
+  },
+});
+const searchBtn = document.querySelector(".mobile-search");
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  channel.destroy();
+  recommended.destroy();
+  food_drink.destroy();
+}
